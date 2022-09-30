019f856a7a53e2a30d8c51f87a6c7de87dd96240
JoannaElHelou <joannahelou01@gmail.com>
2022-09-30 16:53:34 +0200

    premiere question

--- connect4/dumb_ia.py
+++ connect4/dumb_ia.py
@@ -1,6 +1,17 @@
-from .game import Grid, Player
+from .game import Grid, Cell, Player
 
 
-class DumbIA(Player):
+class DumbIA (Player):
     def play(self, grid: Grid) -> int:
-        ...
+        for line in range (grid.lines):
+            for column in range (grid.columns):
+                if grid.grid[line][column] == Cell.EMPTY:
+                    return column
+
+
+
+
+
+
+
+
