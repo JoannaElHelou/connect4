3e4dd97aa58b3f12d5af1c868878fda8f11e1a1c
JoannaElHelou <joannahelou01@gmail.com>
2022-10-05 09:02:04 +0200

    nouveau avec comments

--- connect4/console_player.py
+++ connect4/console_player.py
@@ -2,5 +2,6 @@ from .game import Grid, Player
 
 
 class ConsolePlayer(Player):
+    """Alllow a human to see the grid in its shell, and input a column from the keyboard."""
     def play(self, grid: Grid) -> int:
         ...
