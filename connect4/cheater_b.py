f6797cbb69cb07a7e66caba144d2627316bf0f6f
JoannaElHelou <joannahelou01@gmail.com>
2022-10-05 11:59:24 +0200

    Bonus

--- connect4/cheater_b.py
+++ connect4/cheater_b.py
@@ -1,8 +1,13 @@
-from .game import Grid, Player
+from .game import Grid, Player,Cell
 
 
 class CheaterB(Player):
     """This IA cheats and modify the grid to ensure player B wins."""
 
     def play(self, grid: Grid) -> int:
-        ...
+        print (grid)
+        grid.place(1,Cell.B)
+        grid.place(1,Cell.B)
+        grid.place(1,Cell.B)
+        
+        return 1
